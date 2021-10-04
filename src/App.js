844060a84941bc8d51cf9b6dad5f9c8571013dfd
jungjihyoun <jihyoun0602@gmail.com>
2021-10-04 19:49:58 +0900

    refactor/ 파이어 베이스 연동 notification 확인

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Loading from './components/Loading/Loading';
 import DashBoard from './components/DashBoard/DashBoard';
 import Detect from './components/Detect/Detect';
 import Stretching from './components/Stretching/Stretching';
+import { firebaseApp, vapidKey } from './firebase/firebaseConfig';
+
+import { initializeApp } from 'firebase/app';
+import { onMessage, getMessaging, getToken } from 'firebase/messaging';
 
 function App() {
+	useEffect(() => {
+		initializeApp(firebaseApp);
+		const messaging = getMessaging();
+
+		getToken(messaging, {
+			vapidKey: vapidKey,
+		})
+			.then((currentToken) => {
+				if (currentToken) {
+					// Send the token to your server and update the UI if necessary
+					// ...
+					console.log(currentToken);
+				} else {
+					// Show permission request UI
+					console.log(
+						'No registration token available. Request permission to generate one.',
+					);
+					// ...
+				}
+			})
+			.catch((err) => {
+				console.log('An error occurred while retrieving token. ', err);
+				// ...
+			});
+
+		onMessage(messaging, (payload) => {
+			console.log('Message received. ', payload);
+			var title = payload.notification.title;
+			var options = {
+				body: payload.notification.body,
+				icon: payload.notification.icon,
+			};
+			var notification = new Notification(title, options);
+
+			console.log(options);
+			// ...
+		});
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Switch>
